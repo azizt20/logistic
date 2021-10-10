7df725c00e73b2c5928c600175f111bddc3e6d92
azizt20 <aziiz0120@gmail.com>
2021-10-11 04:51:45 +0500

    сделаны пару страниц менюшки . прописаны роуты. осталось собрать во едино на сдайдбаре

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,60 @@
 <template>
 <div class="settings">
+
+  <a-row type="flex" justify="start">
+    <a-col class="item">
+      <a-button icon="plus-circle"  block type="link" class="button" >
+        Add
+      </a-button>
+    </a-col>
+
+    <a-col class="item">
+      <a-button icon="minus-circle"  block type="link" class="button" >
+        Delete
+      </a-button>
+    </a-col>
+
+    <a-col class="item">
+      <a-button icon="check-circle"  block type="link" class="button" >
+        Check
+      </a-button>
+    </a-col>
+
+    <a-col class="item">
+      <a-button block type="link" class="button"  >
+        Витя логист logist2@mail.ru
+        <a-icon type="check" class="ml-auto" />
+      </a-button>
+    </a-col>
+
+    <a-col class="item">
+      <a-button block type="link" class="button"   >
+        <div>Current value</div>
+        <label  >value</label>
+
+      </a-button>
+    </a-col>
+
+    <a-col class="item">
+      <a-button block type="link" class="button"  >
+        Sub menu
+        <a-icon type="right" class="ml-auto" />
+      </a-button>
+    </a-col>
+
+    <a-col class="item">
+      <a-button block type="link" class="button"  >
+        Reorder
+        <a-icon type="menu" class="ml-auto" />
+      </a-button>
+    </a-col>
+
+    <a-col class="item">
+      <a-button icon="plus-circle"  block type="link" class="button" >
+        Add
+      </a-button>
+    </a-col>
+  </a-row>
 </div>
 </template>
 
@@ -9,6 +64,19 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.item {
+  width: 100%;
+    border-bottom: 1px solid black;
+    text-align: left;
+}
 
+.button {
+  text-align: left;
+  color: black;
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  margin: 22px 0;
+}
 </style>
